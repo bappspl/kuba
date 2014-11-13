17d3c26cd9be51c36c169a99fa1236ddeff0b415
idik91 <idzikkrzysztof91@gmail.com>
2014-11-13 17:43:08 +0100

    Zmiana danych do db

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,7 +2,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=vmadmin_kuba;host=sql.s12.vdl.pl',
+        'dsn'            => 'mysql:dbname=kuba;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
